3f47c8efd4032c0d4ea1f3d8929ad1d5db866089
Ali Mihandoost <i@ali.md>
2016-09-11 19:47:46 +0430

    fix(aliases): remove ip

--- profile.d/aliases.sh
+++ profile.d/aliases.sh
@@ -60,7 +60,7 @@ alias master="git checkout master"
 alias develop="git checkout develop"
 
 # Networking. IP address, dig, DNS
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+# alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 # alias dig="dig +nocmd any +multiline +noall +answer"
 # wget sucks with certificates. Let's keep it simple.
 alias wget="curl -O"
