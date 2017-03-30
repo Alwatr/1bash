3b277a251d195a7d05c02bccba607e024824ea36
Ali Mihandoost <i@ali.md>
2017-03-30 11:13:50 +0430

    feat(prompt): show git tag

--- profile.d/prompt.sh
+++ profile.d/prompt.sh
@@ -85,44 +85,27 @@ set_prompts() {
 
 
     function prompt_git() {
-        # this is >5x faster than mathias's.
-
-        # check if we're in a git repo. (fast)
         git rev-parse --is-inside-work-tree &>/dev/null || return
 
-        # check for what branch we're on. (fast)
-        #   if… HEAD isn’t a symbolic ref (typical branch),
-        #   then… get a tracking remote branch or tag
-        #   otherwise… get the short SHA for the latest commit
-        #   lastly just give up.
-        branchName="$(git symbolic-ref --quiet --short HEAD 2> /dev/null || \
-            git describe --all --exact-match HEAD 2> /dev/null || \
+        # branchName="$(git symbolic-ref --quiet --short HEAD 2> /dev/null || \
+        #     git describe --all --exact-match HEAD 2> /dev/null || \
+        #     git rev-parse --short HEAD 2> /dev/null || \
+        #     echo '(unknown)')";
+
+        branchName="$(\
+            git symbolic-ref --quiet --short HEAD 2> /dev/null) ($(\
+            git describe --tags --exact-match HEAD 2> /dev/null || \
             git rev-parse --short HEAD 2> /dev/null || \
-            echo '(unknown)')";
+            echo '(unknown)' \
+        ))";
 
-
-        ## early exit for Chromium & Blink repo, as the dirty check takes ~5s
-        ## also recommended (via goo.gl/wAVZLa ) : sudo sysctl kern.maxvnodes=$((512*1024))
-        repoUrl=$(git config --get remote.origin.url)
-        if grep -q chromium.googlesource.com <<<$repoUrl; then
-            dirty=" ⁂"
-        else
-
-            # check if it's dirty (slow)
-            #   technique via github.com/git/git/blob/355d4e173/contrib/completion/git-prompt.sh#L472-L475
-            dirty=$(git diff --no-ext-diff --quiet --ignore-submodules --exit-code || echo -e "*")
-
-            # mathias has a few more checks some may like:
-            #    github.com/mathiasbynens/dotfiles/blob/a8bd0d4300/.bash_prompt#L30-L43
-        fi
-
+        dirty=$(git diff --no-ext-diff --quiet --ignore-submodules --exit-code || echo -e "*")
 
         [ -n "${s}" ] && s=" [${s}]";
         echo -e "${1}${branchName}${2}$dirty";
 
         return
     }
-
 
 
     # ------------------------------------------------------------------
