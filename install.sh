8d908e71eb1b0f2870eb453d7ab0bb7a278cbe75
Ali Mihandoost <i@ali.md>
2017-08-19 13:44:10 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cp -v profile.d/* /etc/profile.d/
 
-[ -f ~/.inputrc ] && mv ~/.inputrc ~/.inputrc.bk
+[ -f ~/.inputrc ] && mv -v ~/.inputrc ~/.inputrc.bk
 cp -v .inputrc ~/.inputrc
 
-[ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.bk
+[ -f ~/.bashrc ] && mv -v ~/.bashrc ~/.bashrc.bk
