8bd92820c0cbb7351c17ff4c6a5af3c4b3acd39e
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-11-16 13:57:20 +0330

    feat: setup script with backup

--- README.md
+++ README.md
@@ -1,21 +1,11 @@
-# 1Bash v2.0
+# 1Bash v3.0
 
 Linux Bash Customization, AliMD Edition ;)
 
 ## Install
 
 ```bash
-# clone project
-export ONE_BASH=~/1bash
-git clone https://github.com/AliMD/1bash.git $ONE_BASH
-
-# copy user config
-cp -av $ONE_BASH/inputrc ~/.inputrc
-cp -av $ONE_BASH/bash_profile ~/.bash_profile
-cp -aiv $ONE_BASH/gitconfig ~/.gitconfig
-
-# Edit yout name/email in git config
-nano ~/.gitconfig
+bash <(curl -s https://raw.githubusercontent.com/Alwatr/1bash/v2.3.0/setup.sh)
 ```
 
 ## Customize Logo
@@ -23,16 +13,13 @@ nano ~/.gitconfig
 Put your logo or any welcome message in `~/.1bash_logo`.
 
 ```bash
-nano ~/.1bash_logo
+vim ~/.1bash_logo
 ```
 
 [Generate your own logo](http://patorjk.com/software/taag)
 
 ## Upgrade
 
-Just pull the 1bash git repository.
-
 ```bash
-cd $ONE_BASH
-g pl
+bash ~/$ONE_BASH/setup.sh
 ```
---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+clear
+
+ONE_BASH="$HOME/.1bash"
+ONE_BASH_REPO_URL='https://github.com/Alwatr/1bash'
+ONE_BASH_BRANCH=main
+BACKUP_DIR="$ONE_BASH/backup"
+
+function echoStep() {
+  echo -e "\n\033[1;32m$1\033[0m"
+}
+
+function createSymlink() {
+  local sourceFile=$1
+  local destinationFile=$2
+
+  echo "Creating symbolic link from $sourceFile to $destinationFile"
+
+  if [ -e "$destinationFile" ] && [ ! -L "$destinationFile" ]; then
+    echo "File $destinationFile already exists, creating a backup..."
+    mv $destinationFile $BACKUP_DIR/
+  fi
+
+  ln -svf $sourceFile $destinationFile
+}
+
+# ---
+
+echo -ne "\033[1;33m💎 Setting up 1bash\033[0m\n"
+
+# Clone or update 1bash
+
+if [ -d "$ONE_BASH" ]; then
+  echo '✌🏻 1bash already exists, updating...'
+  cd $ONE_BASH
+  git pull --prune --progress --autostash --rebase
+else
+  echo '✌🏻 1bash does not exist, cloning...'
+  git clone -b $ONE_BASH_BRANCH $ONE_BASH_REPO_URL $ONE_BASH
+fi
+
+# Create symbolic links
+
+echoStep '🔗 Creating symbolic links...'
+mkdir -p $BACKUP_DIR
+createSymlink $ONE_BASH/inputrc.sh ~/.inputrc
+createSymlink $ONE_BASH/bash_profile.sh ~/.bash_profile
+createSymlink $ONE_BASH/gitconfig ~/.gitconfig
+
+echoStep '🎉 Done'
